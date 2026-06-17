8107205311152eb68fdb1d0a45908553504b0bdd
Surya-prathap <13794prathap@gmail.com>
2026-06-17 22:14:16 +0530

    Print elements of an Array

---
+++ src/PrintArrayElements.java
@@ -0,0 +1,13 @@
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5,6,7,8};
+        printElements(nums);
+    }
+
+    public static void printElements(int[] nums){
+        System.out.println("Array Elements are: ");
+        for (int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+}
