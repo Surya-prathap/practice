f1ddc78f82dbf96c7e6a5b793e73598ed327820e
Surya-prathap <13794prathap@gmail.com>
2026-06-17 23:30:41 +0530

    Second smallest element in an Array

---
+++ src/SecondSmallestElement.java
@@ -0,0 +1,20 @@
+public class SecondSmallestElement {
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5,6,7,8};
+        System.out.println(secondSmallest(nums));
+    }
+
+    public static int secondSmallest(int[] nums){
+        int smallest = nums[0];
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int num : nums){
+            if (num < smallest){
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num < secondSmallest && num != smallest) {
+                secondSmallest = num;
+            }
+        }
+        return secondSmallest;
+    }
+}
