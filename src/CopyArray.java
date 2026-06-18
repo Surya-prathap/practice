e76ae023a21cc57a225ed168acb1f681a1e8de11
Surya-prathap <13794prathap@gmail.com>
2026-06-18 14:15:13 +0530

    Copy elements from one Array to another Array

---
+++ src/CopyArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5,6,7};
+        System.out.println(Arrays.toString(copyArray(nums)));
+    }
+
+    public static int[] copyArray(int[] nums){
+        int[] copiedArray = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            copiedArray[i] = nums[i];
+        }
+        return copiedArray;
+    }
+}
